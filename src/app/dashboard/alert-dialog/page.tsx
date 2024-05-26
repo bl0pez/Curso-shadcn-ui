c882d3e9fbb957f0696acf67ff8f358eb2d87e07
bryan lopez <blopez4434@gmail.com>
2024-05-26 13:00:41 -0400

    add componets alert

---
+++ src/app/dashboard/alert-dialog/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+export default function AlertDialogPage() {
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-4 max-w-md">
+      <AlertDialog
+        open={dialogOpen}
+        onOpenChange={setDialogOpen}
+      >
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Button onClick={() => setDialogOpen(true)}>
+        Open Dialog manually
+      </Button>
+    </div>
+  );
+}
